829cabe748cdeb03eb5c519d6384cef4446dfc5e
reneeb <github@renee-baecker.de>
2014-11-19 13:09:53 +0100

    add Config file

---
+++ Kernel/Config/Files/ZZZZDisableOTRSBusinessNotification.pm
@@ -0,0 +1,20 @@
+# OTRS config file (automatically generated)
+# VERSION:1.1
+package Kernel::Config::Files::ZZZZDisableOTRSBusinessNotification;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+
+use List::Util qw(first);
+
+sub Load {
+    my ($File, $Self) = @_;
+
+    my $NotificationModules = $Self->{'Frontend::NotifyModule'};
+    my ($Key)               = first { $_ =~ m{OTRSBusiness} }sort keys %{ $NotificationModules || {} };
+
+    delete $Self->{'Frontend::NotifyModule'}->{$Key};
+}
+
+1;
