20431ab7a01a26a8354d1a4ec0084214304ae4a0
reneeb <info@perl-services.de>
2018-01-26 13:20:00 +0100

    remove debug code

--- var/httpd/htdocs/js/DisableOTRSBusinessNotification.js
+++ var/httpd/htdocs/js/DisableOTRSBusinessNotification.js
@@ -1,7 +1,5 @@
 Core.App.Ready( function() {
     $('.OTRSBusinessRequired').each( function() {
-        console.debug( $(this).parent().nodeName );
-
         if ( $(this).parent().nodeName == "li" ) {
             $(this).parent().remove();
         }
